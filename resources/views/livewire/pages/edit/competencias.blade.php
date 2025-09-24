f63e1b4efa4671cf2731d58c27e3146d60312bff
juan3212 <juanpabloacevedo3212@gmail.com>
2025-09-24 12:08:21 -0500

    se corrige el calculo de notas cuando se cambia una competencia

--- resources/views/livewire/pages/edit/competencias.blade.php
+++ resources/views/livewire/pages/edit/competencias.blade.php
@@ -57,7 +57,7 @@
                 <input type="number" 
                     id="porcentaje" 
                     name="porcentaje" 
-                    wire:model="porcentaje" 
+                    wire:model.live.debounce.200ms="porcentaje" 
                     required
                     class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"> 
             </div>
