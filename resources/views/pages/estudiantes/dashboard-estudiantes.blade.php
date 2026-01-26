17ce5e5e6fed94c1b7654107473ba60b6e0c3ff6
juan3212 <116377243+juan3212@users.noreply.github.com>
2026-01-26 16:06:59 -0500

    ocultar boton de boletines para estudiantes

--- resources/views/pages/estudiantes/dashboard-estudiantes.blade.php
+++ resources/views/pages/estudiantes/dashboard-estudiantes.blade.php
@@ -28,7 +28,7 @@
         </div>
     @endif
     <div class="flex flex-col sm:flex-row justify-center items-center gap-4 mt-8">
-        <a href="{{ route('boletin', $estudianteId) }}" class="block w-11/12 sm:w-64 p-4 text-center text-xl font-bold bg-blue-500 text-white rounded-lg shadow-lg hover:bg-indigo-700 transition duration-300 ease-in-out">
+        <a href="{{ route('boletin', $estudianteId) }}"  class=" hidden block w-11/12 sm:w-64 p-4 text-center text-xl font-bold bg-blue-500 text-white rounded-lg shadow-lg hover:bg-indigo-700 transition duration-300 ease-in-out">
             Boletin
         </a>
         <a href="{{ route('matricula', $estudianteId) }}" class="block w-11/12 sm:w-64 p-4 text-center text-xl font-bold bg-green-500 text-white rounded-lg shadow-lg hover:bg-green-700 transition duration-300 ease-in-out">
