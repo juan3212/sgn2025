8559dd88b8cd058577cb9252d961e1df3ad6d1a3
juan3212 <116377243+juan3212@users.noreply.github.com>
2026-03-05 14:00:13 -0500

    corregir la creacion de roles

--- app/Livewire/Forms/Administrador/RolesForm.php
+++ app/Livewire/Forms/Administrador/RolesForm.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 
 class RolesForm extends Component
 {
-
     public $role;
     public $name;
     public $permissions;
@@ -22,39 +21,40 @@ class RolesForm extends Component
         if ($role) {
             $this->role = Role::find($role);
             $this->name = $this->role->name;
-            $this->permissionSelected = $this->role->permissions->pluck('name')->toArray();
+            $this->permissionSelected = $this->role->permissions
+                ->pluck("name")
+                ->toArray();
         }
     }
-
 
     public function save()
     {
         $this->validate([
-            'name' => 'required|string',
-            'permissions' => 'required|array|min:1',
+            "name" => "required|string",
+            "permissions" => "required|array|min:1",
         ]);
-        
+
         $rolesyPermisosService = new RolesyPermisosService();
         try {
             $role = [
-                'name' => $this->name,
-                'permissions' => $this->permissionSelected
+                "name" => $this->name,
+                "permissions" => $this->permissionSelected,
             ];
             if ($this->role) {
-                $role['role'] = $this->role->name;
+                $role["role"] = $this->role->name;
                 $rolesyPermisosService->updateRole($role);
             } else {
                 $rolesyPermisosService->createRoles($role);
-                $this->reset('name', 'permissionSelected', 'role');
+                $this->reset("name", "permissionSelected", "role");
             }
-            session()->flash('message', 'Role saved successfully');
+            session()->flash("message", "Role saved successfully");
         } catch (\Exception $e) {
-            session()->flash('error', 'Error: ' . $e->getMessage());
+            session()->flash("error", "Error: " . $e->getMessage());
         }
     }
 
     public function render()
     {
-        return view('livewire.forms.administrador.roles-form');
+        return view("livewire.forms.administrador.roles-form");
     }
 }
--- app/Services/RolesyPermisosService.php
+++ app/Services/RolesyPermisosService.php
@@ -14,91 +14,89 @@ class RolesyPermisosService
         //
     }
 
-    public function createRoles(array $request) {
-
+    public function createRoles(array $request)
+    {
         validator($request, [
-            'name' => 'required|string|unique:roles,name',
-            'permissions' => 'required|array|min:1',
+            "name" => "required|string|unique:roles,name",
+            "permissions" => "required|array|min:1",
         ])->validate();
-         
-         DB::beginTransaction();
-         try {
-         $role = Role::create([
-             'name' => $request['name'],
-         ]);
- 
- 
- 
-         $role->syncPermissions($request['permissions']);
- 
-         DB::commit();
-         return response()->json([
-             'success' => true,
-             'message' => 'Role created successfully',
-         ]);
-         } catch (\Exception $e) {
-             DB::rollBack();
-             return response()->json([
-                 'success' => false,
-                 'message' => 'Role not created',
-             ]);
-         }
-     }
- 
-     public function createPermissions(array $request) {
-         validator($request, [
-             'name' => 'required|string',
-         ])->validate();
- 
-         $guard_name = $request['guard_name']??'web';
-         $name = $request['name'];
 
-         Permission::create([
-             'name' => $name,
-             'guard_name' => $guard_name,
-         ]);
- 
-         return response()->json([
-             'success' => true,
-             'message' => 'Permission created successfully',
-         ]);
-     }
- 
-     public function updateRole(array $request) {
+        try {
+            Role::create([
+                "name" => $request["name"],
+            ])->givePermissionTo($request["permissions"]);
+
+            return response()->json([
+                "success" => true,
+                "message" => "Role created successfully",
+            ]);
+        } catch (\Exception $e) {
+            dump($e->getMessage());
+            return response()->json([
+                "success" => false,
+                "message" => "Role not created",
+            ]);
+        }
+    }
+
+    public function createPermissions(array $request)
+    {
+        validator($request, [
+            "name" => "required|string",
+        ])->validate();
+
+        $guard_name = $request["guard_name"] ?? "web";
+        $name = $request["name"];
+
+        Permission::create([
+            "name" => $name,
+            "guard_name" => $guard_name,
+        ]);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Permission created successfully",
+        ]);
+    }
+
+    public function updateRole(array $request)
+    {
         /* Validator::make($request, [
              'role' => 'required|string',
              'name' => 'required|string|unique:roles,name',
              'permissions' => 'required|array|min:1',
          ])->validate();*/
 
-         $role = Role::findByName($request['role']);
-         $role->name = $request['name'];
-         $role->syncPermissions($request['permissions']);
-         $role->save();
- 
-         return response()->json([
-             'success' => true,
-             'message' => 'Permissions updated successfully',
-         ]);
-     }   
- 
-     public function deleteRole($id) {
-         $role = Role::find($id);
-         $role->delete();
- 
-         return response()->json([
-             'success' => true,
-             'message' => 'Role deleted successfully',
-         ]);
-     }
- 
-     public function deletePermission($id) {
-         $permission = Permission::find($id);
-         $permission->delete();
- 
-         return response()->json([
-             'success' => true,
-             'message' => 'Permission deleted successfully',
-         ]);
-     }
+        $role = Role::findByName($request["role"]);
+        $role->name = $request["name"];
+        $role->syncPermissions($request["permissions"]);
+        $role->save();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Permissions updated successfully",
+        ]);
+    }
+
+    public function deleteRole($id)
+    {
+        $role = Role::find($id);
+        $role->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Role deleted successfully",
+        ]);
+    }
+
+    public function deletePermission($id)
+    {
+        $permission = Permission::find($id);
+        $permission->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Permission deleted successfully",
+        ]);
+    }
 }
