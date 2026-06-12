c1b8e23aed62a448997ee3ddcde96d81a1195be7
juan3212 <116377243+juan3212@users.noreply.github.com>
2026-06-12 11:56:22 -0500

    competencias select default “seleccione”

--- resources/views/livewire/forms/competencias-form.blade.php
+++ resources/views/livewire/forms/competencias-form.blade.php
@@ -22,7 +22,7 @@
             wire:model="competenceName"
             required
             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-                <option value="" disabled selected>Seleccione una competencia</option>
+                <option value="" >Seleccione una competencia</option>
                 <option value="C1">C1</option>
                 <option value="C2">C2</option>
                 <option value="C3">C3</option>
