4fb6613c675ba9dd001a2a79e0d4a448cbd1c771
juan3212 <116377243+juan3212@users.noreply.github.com>
2026-06-16 09:46:31 -0500

    bloquear boletines 2do periodo

--- app/Http/Middleware/PaymentStatus.php
+++ app/Http/Middleware/PaymentStatus.php
@@ -61,8 +61,8 @@ class PaymentStatus
         // 2. Usamos el servicio para ver si DEBE ver el boletín
         // Nota: Aquí podrías reutilizar el método hasPaid() del servicio si quieres limpiar más.
         $rutaActualEsPermitida =
-            $request->routeIs("boletin") ||
-            $request->routeIs("matricula") ||
+           // $request->routeIs("boletin") ||
+            //$request->routeIs("matricula") ||
             $request->routeIs("dashboard");
 
         if ($this->paymentService->hasPaid($user) && !$rutaActualEsPermitida) {
